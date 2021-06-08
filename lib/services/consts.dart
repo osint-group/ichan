b4bc7bf0f0e6a189833b7c29fa42c378406465e9
osint-group <kolyakdr@protonmail.com>
2021-06-08 14:38:50 +0300

    Update consts.dart

--- lib/services/consts.dart
+++ lib/services/consts.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:ichan/services/exports.dart';
 import 'package:package_info/package_info.dart';
 
-import 'package:device_info/device_info_plus.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'package:ichan/services/my.dart' as my;
